6ac78f3f41f9620ba32c2fc7d4608fc82945f172
Chia-Chun Wu <ccwu0918@gmail.com>
2022-05-24 02:56:13 +0800

    Update first_page.py

--- first_page.py
+++ first_page.py
@@ -17,7 +17,7 @@ def run(self):
     try:
         if self._target:
             self._target(*self._args, **self._kwargs)
-        finally:
-            # Avoid a refcycle if the thread is running a function with
-            # an argument that has a member that points to the thread.
-            del self._target, self._args, self._kwargs
+    finally:
+    # Avoid a refcycle if the thread is running a function with
+    # an argument that has a member that points to the thread.
+        del self._target, self._args, self._kwargs
