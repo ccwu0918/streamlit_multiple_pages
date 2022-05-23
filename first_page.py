2b1903f6f8d8b8a373b3cfb6d3ce113884bea0f3
Chia-Chun Wu <ccwu0918@gmail.com>
2022-05-24 02:54:46 +0800

    Update first_page.py

--- first_page.py
+++ first_page.py
@@ -12,3 +12,12 @@ state = st.session_state[state_name]
 
 # then reference values with dictionary notation
 state['value'] = "first_page_state"
+
+def run(self):
+    try:
+        if self._target:
+            self._target(*self._args, **self._kwargs)
+        finally:
+            # Avoid a refcycle if the thread is running a function with
+            # an argument that has a member that points to the thread.
+            del self._target, self._args, self._kwargs
